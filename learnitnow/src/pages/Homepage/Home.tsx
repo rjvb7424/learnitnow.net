91be7855e7fc6561e393ef6f8ea9517408c324c6
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-07-08 15:57:40 +0100

    feat: update Home component to include CourseCard in a grid layout

--- learnitnow/src/pages/Homepage/Home.tsx
+++ learnitnow/src/pages/Homepage/Home.tsx
@@ -1,14 +1,31 @@
 // Internal dependencies
-import { Box } from '@mui/material'
-import CourseCard from './components/Coursecard'
+import { Box, Grid } from '@mui/material'
+import CourseCard from './components/CourseCard'
 import CustomAppBar from './components/CustomAppbar'
 
 function Home() {
-
   return (
     <Box>
-      <CustomAppBar/>
-      <CourseCard/>
+      <CustomAppBar />
+
+      {/* Grid Container */}
+      <Box sx={{ p: 3 }}>
+        <Grid 
+          container 
+          spacing={3} 
+          justifyContent="center"
+        >
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Grid 
+              item 
+              xs={12} sm={6} md={4} lg={3} 
+              key={index}
+            >
+              <CourseCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   )
 }
